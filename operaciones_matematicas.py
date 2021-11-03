e2ad0d7da35d5473dc24f45b7c8c0cdab498817d
jeissonomt <93630905+jeissonomt@users.noreply.github.com>
2021-11-03 11:53:43 -0500

    Add files via upload

--- operaciones_matematicas.py
+++ operaciones_matematicas.py
@@ -10,3 +10,14 @@ def restar (n1, n2):
         return n1 - n2
     else:
         return n2 - n1
+
+def parimpar (n1, n2):
+    if(n1 %2 == 0):
+        return n1 " Es par"
+    else:
+        return n1 " Es impar"
+
+    if(n2 %2 == 0):
+        return n2 " Es par"
+    else:
+        return n2 " Es impar"
