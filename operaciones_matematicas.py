cb5eeb5ede5fd665a5a6b2d66c0d11e2974ba293
Andrés F.G. Torres <andres.gonzalez4.mt@correo.usa.edu.co>
2021-11-02 23:35:37 -0500

    Modificación de proyecto base

--- operaciones_matematicas.py
+++ operaciones_matematicas.py
@@ -3,3 +3,26 @@
 # funcion sumar
 def sumar(n1, n2):
     return n1 + n2
+
+def resta (a, b):
+    return print(a - b)
+
+def producto (a, b):
+    return print(a * b)
+
+def division (a, b):
+    return print(a / b)
+
+n, o, m = input("Ingrese dos número junto con su operador separados por espacios: ").split()
+n = float(n)
+m = float(m)
+
+
+if o == "+":
+    suma(n, m)
+elif o == "-":
+    resta(n, m)
+elif o == "*":
+    producto(n, m)
+elif o == "/":
+    division(n, m)
