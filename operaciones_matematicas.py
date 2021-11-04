dca7726f65fecbee596de419e4542c1bda395eca
DanielaAvendano <daniela.avendano.mt@correo.usa.edu.co>
2021-11-04 10:39:06 -0500

    ciclo for - numero del 1 al 10

--- operaciones_matematicas.py
+++ operaciones_matematicas.py
@@ -10,3 +10,9 @@ def restar (n1, n2):
         return n1 - n2
     else:
         return n2 - n1
+
+#Esta linea fue generada por Daniela Avendano a modo de prueba
+#Imprime los numeros del 0 al 10
+for x in range(11):
+    print(x)
+#Fin de la linea
